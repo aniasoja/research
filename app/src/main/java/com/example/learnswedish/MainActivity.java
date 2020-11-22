6aec2ad12e5541db8083c34df1ea009211a7319c
pepinedkova <pepi.nedkova@student.uni-tuebingen.de>
2020-11-22 20:21:55 +0100

    Notifications first steps done

--- app/src/main/java/com/example/learnswedish/MainActivity.java
+++ app/src/main/java/com/example/learnswedish/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.learnswedish;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,4 +38,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+//    FirebaseMessaging.getInstance().getToken()
+//    .addOnCompleteListener(new OnCompleteListener<String>() {
+//        @Override
+//        public void onComplete(@NonNull Task<String> task) {
+//            if (!task.isSuccessful()) {
+//                Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+//                return;
+//            }
+//
+//            // Get new FCM registration token
+//            String token = task.getResult();
+//
+//            // Log and toast
+//            String msg = getString(R.string.msg_token_fmt, token);
+//            Log.d(TAG, msg);
+//            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//        }
+//    });
 }
---
+++ app/src/main/java/com/example/learnswedish/MyFirebaseMessagingService.java
@@ -0,0 +1,43 @@
+package com.example.learnswedish;
+
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+import androidx.annotation.NonNull;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "TagName";
+
+    /**
+     * Called if FCM registration token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the
+     * FCM registration token is initially generated so this is where you would retrieve
+     * the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+        System.out.println(token);
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    /**
+     * Persist token to third-party servers.
+     *
+     * Modify this method to associate the user's FCM InstanceID token with any server-side account
+     * maintained by your application.
+     *
+     * @param token The new token.
+     */
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+}
+
