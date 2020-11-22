2a74a8df69a59c96d974ecb147f378e38e58f42e
aniasoja <annaj.soja@gmail.com>
2020-11-23 00:44:18 +0100

    checking the date and amount of words to learn

--- app/src/main/java/com/example/learnswedish/LearnActivity.java
+++ app/src/main/java/com/example/learnswedish/LearnActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class LearnActivity extends AppCompatActivity {
 
@@ -25,16 +29,29 @@ public class LearnActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
 
+        Log.d("words_learnt", "getting in");
+
         int words_learnt = getIntent().getIntExtra("words_learnt", 0);
+        Log.d("words_learnt", String.valueOf(words_learnt));
 
         btnLearnt = findViewById(R.id.btnLearnt);
         btnLearnt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int wl = words_learnt+3;
+                int today = Calendar.DAY_OF_MONTH;
                 SharedPreferences.Editor editor = getSharedPreferences(WORDS_LEARNT, MODE_PRIVATE).edit();
                 editor.putInt("words_learnt", wl);
+                editor.putInt("lastStudied", today);
+                Log.d("words_learnt", String.valueOf(wl));
                 editor.commit();
+                SharedPreferences getWords = getSharedPreferences(WORDS_LEARNT, MODE_PRIVATE);
+                int wl2 = getWords.getInt("words_learnt", 0);
+                Log.d("words_learnt", String.valueOf(wl2));
+
+                Intent back = new Intent(LearnActivity.this, com.example.learnswedish.MainActivity.class);
+                back.putExtra("words_learnt", wl);
+                startActivity(back);
             }
         });
 
@@ -52,6 +69,12 @@ public class LearnActivity extends AppCompatActivity {
         words.add(new Word("a waiter", "en servitör"));
         words.add(new Word("policeman", "en polis"));
         words.add(new Word("firefighter", "en brandman"));
+        words.add(new Word("I cook", "jag lagar mat"));
+        words.add(new Word("I bake", "jag bakar"));
+        words.add(new Word("a pot", "en svett"));
+        words.add(new Word("a pan", "en man"));
+        words.add(new Word("a microvawe", "en mikrovågsugn"));
+        words.add(new Word("a corkscrew", "en korkskruv"));
 
         myAdapter = new WordAdapter(this, words, words_learnt);
 
--- app/src/main/java/com/example/learnswedish/MainActivity.java
+++ app/src/main/java/com/example/learnswedish/MainActivity.java
@@ -5,29 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLearn;
-    public static final String WORDS_LEARNT = "com.example.learnswedish.wordslearnt";
+    public static final String WORDS_LEARNT = "com.example.learnswedish.wordslearnt"; //adress of the file with stored data
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        SharedPreferences getWords = getSharedPreferences(WORDS_LEARNT, MODE_PRIVATE);
-        int words_learnt = getWords.getInt("words_learnt", 0);
-
         setContentView(R.layout.activity_main);
-        btnLearn = findViewById(R.id.btnStart);
-        btnLearn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent learn = new Intent(MainActivity.this, com.example.learnswedish.LearnActivity.class);
-                learn.putExtra("words_learnt", words_learnt);
-                startActivity(learn);
+
+         //button Lean setup
+         btnLearn = findViewById(R.id.btnStart);
+         btnLearn.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+                 //get the dat of the last lesson done and compare with today's data
+                 int today = Calendar.DAY_OF_MONTH;
+                 Log.d("today", String.valueOf(today));
+
+                 SharedPreferences getDay = getSharedPreferences(WORDS_LEARNT, MODE_PRIVATE);
+                 int lastDay = getDay.getInt("lastStudied", 0);
+                 Log.d("lastDay",  String.valueOf(lastDay));
+
+                 if (today == lastDay && lastDay != 0){ //if studied today
+                     Toast.makeText(MainActivity.this, "You've learnt our top 3 words today", Toast.LENGTH_SHORT).show();
+                     Log.d("yes", "yes");
+
+                 } else {
+
+                     //get amount of words already learnt
+                     int words_learnt;
+                     SharedPreferences getWords = getSharedPreferences(WORDS_LEARNT, MODE_PRIVATE);
+                     words_learnt = getWords.getInt("words_learnt", 0);
+
+                     //get amount of the words learnt stored in the app
+                    int wlIntent = getIntent().getIntExtra("words_learnt", 0);
+
+                     Log.d("words_learnt", String.valueOf(words_learnt));
+
+                     if (words_learnt != wlIntent && wlIntent != 0){
+                         Toast.makeText(MainActivity.this, "You've learnt our top 3 words today", Toast.LENGTH_SHORT).show();
+                     } else {
+                         Log.d("words_learnt", "going in");
+                         Intent learn = new Intent(MainActivity.this, com.example.learnswedish.LearnActivity.class);
+                         learn.putExtra("words_learnt", words_learnt);
+                         startActivity(learn);
+                     }
+                }
             }
         });
     }
