b7772044503cd67b0411f83a7bf7ca94e2a3dd6c
pepinedkova <pepi.nedkova@student.uni-tuebingen.de>
2020-11-22 21:12:44 +0100

    Notifications small improvement

--- app/src/main/java/com/example/learnswedish/MainActivity.java
+++ app/src/main/java/com/example/learnswedish/MainActivity.java
@@ -9,13 +9,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     Button btnLearn;
     public static final String WORDS_LEARNT = "com.example.learnswedish.wordslearnt";
@@ -37,24 +39,27 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(learn);
             }
         });
+
+//        final String TAG = "TagName";
+//        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+//            @Override
+//            public void onComplete(@NonNull Task<String> task) {
+//                if (!task.isSuccessful()) {
+//                    Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+//                    return;
+//                }
+//
+//                // Get new FCM registration token
+//                String token = task.getResult();
+//
+//                // Log and toast
+//                String msg = getString(R.string.msg_token_fmt, token);
+//                Log.d(TAG, msg);
+//                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
-//    FirebaseMessaging.getInstance().getToken()
-//    .addOnCompleteListener(new OnCompleteListener<String>() {
-//        @Override
-//        public void onComplete(@NonNull Task<String> task) {
-//            if (!task.isSuccessful()) {
-//                Log.w(TAG, "Fetching FCM registration token failed", task.getException());
-//                return;
-//            }
-//
-//            // Get new FCM registration token
-//            String token = task.getResult();
-//
-//            // Log and toast
-//            String msg = getString(R.string.msg_token_fmt, token);
-//            Log.d(TAG, msg);
-//            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
-//        }
-//    });
+
+
 }
