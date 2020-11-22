09f0588e85e7f6c57197edbf9636fc5730be76fc
pepinedkova <pepi.nedkova@student.uni-tuebingen.de>
2020-11-22 01:09:55 +0100

    Main Activity Layout Constrained

--- app/src/main/java/com/example/learnswedish/MainActivity.java
+++ app/src/main/java/com/example/learnswedish/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
 
-        btnLearn = findViewById(R.id.btnLearn);
+        btnLearn = findViewById(R.id.btnStart);
         btnLearn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
